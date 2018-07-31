4e39780dec53351e1c6c1216f72a3289cd7bc65a
xhizor <mateo.mlikota@student.fsr.ba>
2018-07-31 17:48:08 +0200

    Django Debug Toolbar Configuration

--- rent_a_car/settings.py
+++ rent_a_car/settings.py
@@ -120,3 +120,27 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+# Django Debug Toolbar Configuration
+
+if DEBUG:  # Use local only
+    INSTALLED_APPS += 'debug_toolbar',
+    MIDDLEWARE += 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
+
+    DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS': False, }
+
+    INTERNAL_IPS = ('127.0.0.1', 'localhost',)
