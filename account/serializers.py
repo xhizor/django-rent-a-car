6b30ab69e3bdb94175becfcbff1ca8412ef88b48
xhizor <mateo.mlikota@student.fsr.ba>
2018-08-01 22:05:54 +0200

    Revert "Create Register View"
    This reverts commit 998b6a1

--- account/serializers.py
+++ account/serializers.py
@@ -1,24 +1,37 @@
 from django.contrib.auth.models import User
+from datetime import datetime
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
-#from .models import Profile
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from .models import UserProfile
+
+
+class UserProfileSerializer(ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('birth_date', 'address', 'photo')
 
 
 class UserSerializer(ModelSerializer):
+    user_profile = UserProfileSerializer(required=False)
+
     class Meta:
-        #model = Profile
-        fields = ['username', 'email', 'password', 'first_name',
-                  'last_name', 'address', 'photo']
-
+        model = User
+        fields = ('username', 'email', 'password', 'first_name',
+                  'last_name', 'user_profile')
+    
+    def validate(self, data):
+        if datetime.now().year - data['user_profile'].get('birth_date').year < 18:
+            raise ValidationError('Only 18+ users are able to register!')
+        return data
+    
     def create(self, data):
-        user = User.objects.create(username=data.get('username'),
-                                   email=data.get('email'),
-                                   first_name=data.get('first_name'),
-                                   last_name=data.get('last_name'))
+        user_profile = data.pop('user_profile')
+        user = User.objects.create(**data)
         user.set_password(data.get('password'))
-        user_obj = user.save(commit=False)
-        #profile = Profile.objects.create(user=user_obj, address=data.get('address'),
-         #                                photo=data.get('photo'))
+        user.save()
+        UserProfile.objects.create(user=user, **user_profile)
         return user
-
 
 
--- account/views.py
+++ account/views.py
@@ -1,10 +1,15 @@
-from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+from .models import UserProfile
 from .serializers import UserSerializer
 
 
 class CreateUserView(CreateAPIView):
-    model = User
+    model = UserProfile
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+
+
+
